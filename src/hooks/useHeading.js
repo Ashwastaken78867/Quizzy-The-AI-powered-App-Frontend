d0bfc1aa219034a5c142411e127afc977b1bc447
Bagda@Sh <168397146+Ashwastaken78867@users.noreply.github.com>
2025-09-29 10:40:10 +0530

    Update heading

--- src/hooks/useHeading.js
+++ src/hooks/useHeading.js
@@ -1,7 +1,9 @@
 import { useSelector } from "react-redux";
 import { commonSelector } from "../store/features/common/selectors";
 import { useDispatch } from "react-redux";
+// import { setCommonKey } from "../store/features/common/commonSlice";
 import { setCommonKey } from "../store/features/common/commonSlice";
+
 
 export const useHeading = () => {
   // const dispatch = useDispatch();
